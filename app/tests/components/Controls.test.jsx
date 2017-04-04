db25c82b8e5a4caa7111a4b5c58c6503ff3ff2a7
djstojadin <djstojadin@gmail.com>
2017-04-04 23:58:46 +0200

    Add static controls component

---
+++ app/tests/components/Controls.test.jsx
@@ -0,0 +1,33 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtilis = require('react-addons-test-utils');
+
+var Controls = require('Controls');
+
+describe('Controls', () => {
+  it('should exist', () => {
+    expect(Controls).toExist();
+  });
+
+  describe('render', () => {
+    it('should render pause when started', () => {
+      var controls = TestUtilis.renderIntoDocument(<Controls countdownStatus="started"/>);
+      var $el = $(ReactDOM.findDOMNode(controls));
+      var $pauseButton = $el.find('button:contains(Pause)');
+
+      expect($pauseButton.length).toBe(1);
+    });
+  });
+
+  describe('render', () => {
+    it('should render start when paused', () => {
+      var controls = TestUtilis.renderIntoDocument(<Controls countdownStatus="paused"/>);
+      var $el = $(ReactDOM.findDOMNode(controls));
+      var $pauseButton = $el.find('button:contains(Start)');
+
+      expect($pauseButton.length).toBe(1);
+    });
+  });
+});
